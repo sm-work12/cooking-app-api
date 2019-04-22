5d9ffe8d12e03308008f08de9fdf639b45d1c7cf
Syed Shahadat Sayem <sm.work12@gmail.com>
2019-04-22 13:14:07 +0600

    Add static root for deploying on heroku

--- app/app/settings.py
+++ app/app/settings.py
@@ -125,5 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(os.path.dirname(BASE_DIR),'static')
 
 AUTH_USER_MODEL='core.User'
